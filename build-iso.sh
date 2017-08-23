9141954bb0574952fca23428dc1002ec1fd61313
Vince van Oosten <vince.vanoosten@divider.net>
2017-08-23 10:52:13 +0200

    add script to build automatically

---
+++ build-iso.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+which vagrant >/dev/null
+which git     >/dev/null
+
+git clone https://github.com/nixos/nixpkgs nixpkgs --depth 1 || true
+vagrant up || true
+
+cd nixpkgs/nixos/modules/installer/cd-dvd && ln -sf ../../../../../techhazard && cd -
+sed -i'' -e "s!\(./installation-cd-base.nix\)!\1\n      ./techhazard/techhazard.nix!" nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix
+
+vagrant ssh -c 'cd /vagrant/nixpkgs/nixos; nix-build -A config.system.build.isoImage -I nixos-config=modules/installer/cd-dvd/installation-cd-minimal.nix default.nix && cp -f result/iso/*.iso /vagrant/ && sync'
+vagrant halt
+#vagrant destroy -f
+
+exit 0
